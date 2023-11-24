5085048501b20362769b5f09c4be3f111e710aef
DhanushaSuharshana <dinudhanusha@gmail.com>
2023-11-24 09:10:25 +0530

    #3 Remove unnecessary files

--- admin/ajax/php/log-out.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once('../../../class/include.php');
-$USER = new User(NULL);
-
-if ($USER->logOut()) {
-    header('Location: ../../login.php');
-} 
-
--- admin/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-$USER = New User(NULL);
-
-if (!isset($_SESSION)) {
-    session_start();
-} 
-if (!$USER->authenticate()) {
-    redirect('login.php'); 
-}
