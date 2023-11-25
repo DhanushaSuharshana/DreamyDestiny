a9a7ca26fb96511d96931441b7c0169bdff17cb7
sachini yureaka <yureaka97@gmail.com>
2023-11-25 12:12:22 -0800

    #22 Front End: Import sections to home page

---
+++ include/footerJS.php
@@ -0,0 +1,21 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Flex Nav Script -->
+<script src="js/jquery.flexnav.js" type="text/javascript"></script>
+<script src="js/navigation.js"></script>
+<!-- Flex Nav Script for venue listing -->
+<script type="text/javascript" src="js/bootstrap-select.js"></script>
+<script src="js/jquery.sticky.js"></script>
+<script src="js/header-sticky.js"></script>
+<script src="../../../code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+<script type="text/javascript" src="js/price-slider.js"></script>
+<!-- slider -->
+<script src="js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="js/slider.js"></script>
+<!-- testimonial -->
+<script type="text/javascript" src="js/testimonial.js"></script>
+<!-- sticky header -->
+<script src="js/jquery.sticky.js"></script>
+<script src="js/header-sticky.js"></script>
